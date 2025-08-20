417c61414e1a23a6a716928a0cce64fb20402e64
Leo cappiello <leonelcappiello@gmail.com>
2025-08-20 14:23:40 -0300

    First commit !

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { DateModule } from './date/date.module';
+import { ClientModule } from './client/client.module';
+import { PaymentModule } from './payment/payment.module';
+
+@Module({
+  imports: [AuthModule, DateModule, ClientModule, PaymentModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
