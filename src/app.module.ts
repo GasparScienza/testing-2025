d5b94e74104209e9b2b9280185d5de1828109782
Leo cappiello <leonelcappiello@gmail.com>
2025-08-27 12:33:11 -0300

    Agrego modulos y validaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { AuthModule } from './auth/auth.module';
 import { DateModule } from './date/date.module';
 import { ClientModule } from './client/client.module';
 import { PaymentModule } from './payment/payment.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, DateModule, ClientModule, PaymentModule],
+  imports: [
+    AuthModule,
+    DateModule,
+    ClientModule,
+    PaymentModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
